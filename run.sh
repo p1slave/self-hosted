fd1d72fbcb6e606ce6f55cad52ed93c119dbc55b
p1slave <p1slave@protonmail.com>
2022-04-06 02:27:00 -0500

    Add mailu to docker-compose configs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker network create external-self-hosted-net
-docker-compose -p self-hosted -f listmonk/docker-compose.yml -f caddy/docker-compose.yml --env-file .env up -d
+docker-compose -p self-hosted -f listmonk/docker-compose.yml -f caddy/docker-compose.yml -f mailu/docker-compose.yml --env-file .env up -d
 # docker-compose -p self-hosted -f listmonk/docker-compose.yml -f caddy/docker-compose.yml stop
